65fbef748a63a1cd379300261b431ed6abd200e8
Crowdbotics <team@crowdbotics.com>
2024-03-02 01:34:02 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "bitter-lab-46765",
+  url: "https://bitter-lab-46765.botics.co",
+  api: "https://bitter-lab-46765.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
